7768095f8e318fdc66706b6ad30c6250e167f449
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 16:12:02 -0400

    Final Ui product with help

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -11,6 +11,43 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             Image("background-cloth")
+            
+            VStack{
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Spacer()
+                Image("button")
+                Spacer()
+                HStack{
+                    Spacer()
+                    VStack{
+                        Text("PLAYER")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack{
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10.0)
+                        Text("0")
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
     }
 }
