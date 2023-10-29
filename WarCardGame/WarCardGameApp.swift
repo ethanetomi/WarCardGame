6f91390b3103323abdb490dda180679d4af93e4d
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 15:15:35 -0400

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Ethan Etomi on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
