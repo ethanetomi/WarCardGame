ce95a88a45d642cab3e02728d01ce6d3b3cee608
Ethan Etomi <ethanetomi@icloud.com>
2023-10-29 15:53:52 -0400

    Beginning with help

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -10,36 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-            Image("background-wood-grain")
-                .ignoresSafeArea()
-            VStack{
-                Spacer()
-                Image("logo")
-                HStack{
-                    Spacer()
-                    Image("card2")
-                    Spacer()
-                    Image("card10")
-                    Spacer()
-                    
-                }
-                Spacer()
-                Image("button")
-                HStack{
-                    Spacer()
-                    Text("Player")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                    Text("CPU")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    Spacer()
-                }
-                Spacer()
-                
-                
-            }
+            Image("background-cloth")
         }
-        
     }
 }
 
